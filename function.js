2c651f97657840355d1fe6f2739c5819b4240dfc
ccholfo90 <127322452+ccholfo90@users.noreply.github.com>
2023-03-08 23:09:12 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,9 @@
+//muchas gracias por todo lo enseñado
+
+console.log("hola mundo")
+
+document.getElementById('Contacto').onclick = function () {
+    console.log("tienen mi contacto");
+    document.getElementById("contact").innerHTML = "Mi contacto es +5478999122"
+    
+};
