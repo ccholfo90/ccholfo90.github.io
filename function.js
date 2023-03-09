76ccc037074d8029d95dae393fb35b0d826209a4
ccholfo90 <127322452+ccholfo90@users.noreply.github.com>
2023-03-09 19:16:55 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -5,5 +5,4 @@ console.log("hola mundo")
 document.getElementById('Contacto').onclick = function () {
     console.log("tienen mi contacto");
     document.getElementById("contact").innerHTML = "Mi contacto es +5478999122"
-    
-};
+    };
